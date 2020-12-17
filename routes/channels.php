470c6b4f57f190827ce95e1450b6d058e2bfc216
Dominik R Wojtasiewicz <dominikwojtasiewicz@gmail.com>
2020-12-17 13:18:35 +0000

    Added correct listiner notifications

--- routes/channels.php
+++ routes/channels.php
@@ -19,5 +19,5 @@ Broadcast::channel('App.User.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('user.{userId}', function ($user, $userId) {
-    return (int) $user->id === (int) $id;
+    return (int) $user->id === (int) $userId;
 });
