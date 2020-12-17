26db364882bfd6a8254fddefa3aef475a46008ec
Dominik R Wojtasiewicz <dominikwojtasiewicz@gmail.com>
2020-12-17 10:26:01 +0000

    Added pusher and configuration

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,3 @@
-import Echo from "laravel-echo";
 
 window._ = require('lodash');
 
@@ -10,16 +9,10 @@ window._ = require('lodash');
 
 try {
     window.Popper = require('popper.js').default;
-    window.Pusher = require('pusher-js');
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
 } catch (e) {}
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'your-pusher-channels-key'
-});
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -37,13 +30,13 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    forceTLS: true
+});
